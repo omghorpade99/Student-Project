1fa4fba5b0e60e4487f307d87e85da5d39deef2d
Omrane497 <147480588+Omrane497@users.noreply.github.com>
2023-10-10 15:05:42 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-
-  int first_number, second_number, sum;
+//snnsbuvhsvck 
+int first_number, second_number, sum;
     
   cout << "Enter two integers: ";
   cin >> first_number >> second_number;
